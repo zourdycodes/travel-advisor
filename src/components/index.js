3a5bc9d4f3f4981fb09022c53f954bd208fed165
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-01 18:46:52 +0700

    :building_construction: creating map and render the map
    on-going

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-// export { Map } from "./Map";
-// export { List } from "./List";
+export { Map } from './Map';
+export { List } from './List';
 export { Header } from './Header';
 // export { PlaceDetails } from "./PlaceDetails";
